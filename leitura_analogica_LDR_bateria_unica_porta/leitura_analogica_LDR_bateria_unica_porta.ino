bee5b198114b4666eae62ffda6b547bdc0a477ea
Alberio Lima <alberio@dadox.com.br>
2018-01-18 00:35:02 -0300

    Leitura LDR+bateria na porta A0

--- leitura_analogica_LDR_bateria_unica_porta/leitura_analogica_LDR_bateria_unica_porta.ino
+++ leitura_analogica_LDR_bateria_unica_porta/leitura_analogica_LDR_bateria_unica_porta.ino
@@ -1,3 +1,7 @@
+/* 
+ * Exemplo de como ler dois dispositivos na mesma porta analógica
+ * Aqui estamos lendo um LDR e a tensão da bateria que alimenta o sistema
+ */
 #define pinAtivaLDR  2 //porta digital que aciona a leitura do LDR
 #define pinAtivaBAT  3 //porta digital que aciona a leitura da bateria
 #define pinAnalogico A0 //Unica porta usada para leitura
@@ -5,29 +9,32 @@
 boolean lerLDR;
 void setup() {
   Serial.begin( 9600 );
-  pinMode( pinAtivaBAT, OUTPUT );
-  pinMode( pinAnalogico, INPUT );
-  pinMode( pinAtivaLDR, OUTPUT );
+  pinMode( pinAnalogico, INPUT );  //Configura a porta de leitura
+  pinMode( pinAtivaBAT, OUTPUT );  //Configura porta que ativa a leitura da bateria como saída
+  pinMode( pinAtivaLDR, OUTPUT );  //Configura porta que ativa a leitura do LDR como saída
   
   //Inicia lendo LDR
   lerLDR = true;
-  digitalWrite( pinAtivaLDR, HIGH);
-  digitalWrite( pinAtivaBAT, LOW);  
+  digitalWrite( pinAtivaLDR, HIGH); //Leitura do LDR ativada
+  digitalWrite( pinAtivaBAT, LOW);  //Leitura da bateria desativada
 }
 
 void loop() {
-  if ( Serial.available() > 0 ) {
-    Serial.read();
-    lerLDR = !lerLDR;
-    if (lerLDR){
+  if ( Serial.available() > 0 ) { //Quando qualquer coisa vier pela serial é feita a alternancia entre LDR e bateria
+    while ( Serial.available() > 0 ) {
+      Serial.read();
+    }
+    lerLDR = !lerLDR;a 
+    if (lerLDR){ //Ativa leitura LDR
       digitalWrite( pinAtivaBAT, LOW );
       pinMode( pinAtivaLDR, OUTPUT );
       digitalWrite( pinAtivaLDR, HIGH );
-    } else {
+    } else { //Ativa leitura bateria
       pinMode( pinAtivaLDR, INPUT );
       digitalWrite( pinAtivaBAT, HIGH );      
     }
   }
+  //Mostra os dados pela Serial
   int valLido = analogRead( pinAnalogico );
   Serial.print(  valLido );
   Serial.print( " " );
