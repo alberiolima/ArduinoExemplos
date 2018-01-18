b8428b24985c29082236438af2f279ccef795500
Alberio Lima <alberio@dadox.com.br>
2018-01-18 00:24:44 -0300

    Leitura LDR+bateria na porta A0

---
+++ leitura_analogica_LDR_bateria_unica_porta/leitura_analogica_LDR_bateria_unica_porta.ino
@@ -0,0 +1,37 @@
+#define pinAtivaBAT  A4 //porta digital que aciona a leitura da bateria
+#define pinAtivaLDR  A5 //porta digital que aciona a leitura do LDR
+#define pinAnalogico A0 //Unica porta usada para leitura
+
+boolean lerLDR;
+void setup() {
+  Serial.begin( 9600 );
+  pinMode( pinAtivaBAT, OUTPUT );
+  pinMode( pinAnalogico, INPUT );
+  pinMode( pinAtivaLDR, OUTPUT );
+  
+  //Inicia lendo LDR
+  lerLDR = true;
+  digitalWrite( pinAtivaLDR, HIGH);
+  digitalWrite( pinAtivaBAT, LOW);  
+}
+
+void loop() {
+  if ( Serial.available() > 0 ) {
+    Serial.read();
+    lerLDR = !lerLDR;
+    if (lerLDR){
+      digitalWrite( pinAtivaBAT, LOW );
+      pinMode( pinAtivaLDR, OUTPUT );
+      digitalWrite( pinAtivaLDR, HIGH );
+    } else {
+      pinMode( pinAtivaLDR, INPUT );
+      digitalWrite( pinAtivaBAT, HIGH );      
+    }
+  }
+  int valLido = analogRead( pinAnalogico );
+  Serial.print(  valLido );
+  Serial.print( " " );
+  Serial.print(  (float)valLido * (5.0 / 1024.0), 2 );
+  Serial.println();
+  delay(50);
+}
