205e085cf2ba9728a82de05b53bf8f5a2747c6c4
Alberio Lima <alberio@dadox.com.br>
2018-02-23 14:52:59 -0300

    timer_serial

--- timer_serial/timer_serial.ino
+++ timer_serial/timer_serial.ino
@@ -1,72 +1,86 @@
 #define incrementoTimer 1000
-String sTempo = "";
-unsigned long iTimer = 0; 
-boolean timeAtivo = false;
+
+//Variáveis públicas
+unsigned long iTimerAtual = 0; 
 unsigned long proxAtualizacao = 0;
+boolean timeAtivo = false;
+String sTempo = "";
 
 void setup() {
-  Serial.begin( 9600 );
+  Serial.begin( 9600 ); //Inicia comunicação serial
   Serial.println( "Iniciado!" );
 }
 
 void loop() {
+  
+  //Decrementar timer quando ativo
   if ( (timeAtivo)&&(millis() >= proxAtualizacao) ) {
-    proxAtualizacao = millis() + incrementoTimer;
-    iTimer -= incrementoTimer;
-    if ( iTimer == 0  ) {
+    proxAtualizacao += incrementoTimer;
+    iTimerAtual -= incrementoTimer;
+    
+    //Desativa o timer quando concluir o tempo
+    if ( iTimerAtual == 0  ) {
       timeAtivo = false;
       Serial.println( "concluido" );
-    }
-    
+    }    
   }
+  
   if ( Serial.available() > 0 ) {
-    while ( Serial.available() > 0 ) {
-      char cLido = Serial.read();
-      if ( cLido == '\n' ) {                                                              //012345678\n      
-        if ((timeAtivo)&&( sTempo[0] == 'T' )) {
-          Serial.print( "timer já ativo" );
-        } else  if (( sTempo[0] == 'T' )&&(sTempo[3] == ':')&&(sTempo[6] == ':')) { //T00:00:00\n
-          String sHora, sMinuto, sSegundos;
-          sHora = sTempo.substring( 1, 3 );
-          sMinuto = sTempo.substring( 4, 6 );
-          sSegundos = sTempo.substring( 7, 9 );
-          iTimer = sHora.toInt() * 3600;
-          iTimer = iTimer + ( sMinuto.toInt() * 60 );
-          iTimer = iTimer + sSegundos.toInt();
-          iTimer = iTimer * 1000;
-          timeAtivo = true;
-          proxAtualizacao = 0;
-        }
-        sTempo = ""; 
-      } else if (( cLido == 'C' )&&(timeAtivo)) {  
-        timeAtivo = false;
-        Serial.print( "\nTimer cancelado");
-      } else if (( cLido == 'S' )) {  
-        if (timeAtivo) {
-          Serial.print( '\n' );
-          int iHoras = 0;
-          int iMinutos = 0;    
-          unsigned long auxTime = iTimer / 1000;    
-          if ( auxTime >= 3600 ) {
-            iHoras = (int)(auxTime / 3600);
-            auxTime = auxTime - ( (unsigned long)iHoras * 3600UL );
-          }
-          if ( auxTime >= 60 ) {
-            iMinutos = (int)(auxTime / 60);
-            auxTime = auxTime - ( (unsigned long)iMinutos * 60UL );
-          }
-          Serial.print( "A" );
-          Serial.print( iHoras );
-          Serial.print( ":" );
-          Serial.print( iMinutos );
-          Serial.print( ":" );
-          Serial.print( auxTime );
-        } else {
-          Serial.println( "Pronto" );
-        }
-      } else {
-        sTempo = sTempo + cLido;
+    char cLido = Serial.read();
+    if ( cLido == '\n' ) {                                                              
+      if ((timeAtivo) && ( sTempo[0] == 'T' )) {
+        Serial.println( "timer ja ativo" );
+      } else  if (( sTempo[0] == 'T' )&&(sTempo[3] == ':')&&(sTempo[6] == ':')) { //T00:00:00
+        iTimerAtual = strToMillis(sTempo.substring(1));
+        //Inicia a contagem
+        timeAtivo = true;
+        proxAtualizacao = millis();
       }
+      sTempo = ""; 
+    } else if (( cLido == 'C' )&&(timeAtivo)) {  
+      timeAtivo = false;
+      Serial.println( "Timer cancelado");
+    } else if (( cLido == 'S' )) {  
+      if (timeAtivo) {        
+        Serial.print( "A" );
+        Serial.println( millisToStr( iTimerAtual) );
+      } else {
+        Serial.println( "Pronto" );
+      }
+    } else {
+      sTempo = sTempo + cLido;
     }
   }
+
 }
+
+unsigned long strToMillis( String tempo ) {
+  //Separa hms
+  String sHora, sMinuto, sSegundos;
+  sHora = tempo.substring( 0, 2 );
+  sMinuto = tempo.substring( 3, 5 );
+  sSegundos = tempo.substring( 6, 8 );
+        
+  //Converte hms para millissegundos
+  unsigned long ret = 0;
+  ret += sHora.toInt() * 3600;
+  ret += ( sMinuto.toInt() * 60 );
+  ret += sSegundos.toInt();  
+  return (ret * 1000UL);  
+}
+
+String millisToStr( unsigned long ulTempo ) {
+  int iHoras = 0;
+  int iMinutos = 0;    
+  ulTempo /= 1000;    
+  if ( ulTempo >= 3600 ) {
+    iHoras = (int)(ulTempo / 3600);
+    ulTempo -= ( (unsigned long)iHoras * 3600UL );
+  }
+  if ( ulTempo >= 60 ) {
+    iMinutos = (int)(ulTempo / 60);
+    ulTempo -= ( (unsigned long)iMinutos * 60UL );
+  }
+  return ( String( iHoras) + ":" + String( iMinutos ) + ":" + String( ulTempo) );
+}
+
