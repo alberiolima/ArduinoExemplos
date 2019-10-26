e3e3b9b61e32bb423125b7a8a61359d45e91d404
Alberio Lima <alberio@dadox.com.br>
2019-10-26 09:46:08 -0300

    Repeticao de sinal

--- repeticao_sinal_px/repeticao_sinal_px.ino
+++ repeticao_sinal_px/repeticao_sinal_px.ino
@@ -1,7 +1,7 @@
 #define PIN_LEITURA 2
 #define PIN_GRAVAR  3
 
-#define sinalLer    HIGH
+#define sinalLer    LOW
 #define sinalGravar HIGH
 
 unsigned long tempoIniciou = 0;
@@ -21,25 +21,37 @@ void setup() {
 
 void loop() {
   //Faz leitura do sinal
-  if ((digitalRead( PIN_LEITURA ) == sinalLer) && (tempoIniciou == 0)) 
+  if ((digitalRead( PIN_LEITURA ) == sinalLer) && (tempoIniciou == 0))
   {
-    tempoIniciou = millis(); //Pega o tempo inicial do recebimento do sinal de entrada
-    Serial.print( "Recebendo sinal de entrada, " );
-  } 
-  else if ((digitalRead( PIN_LEITURA ) != sinalLer) && (tempoIniciou > 0)) 
+    delay(5);//debounce
+    if (digitalRead( PIN_LEITURA ) == sinalLer)
+    {
+      tempoIniciou = millis(); //Pega o tempo inicial do recebimento do sinal de entrada
+      Serial.println();
+      Serial.print( "Recebendo sinal de entrada, " );
+    }
+  }
+  else if ((digitalRead( PIN_LEITURA ) != sinalLer) && (tempoIniciou > 0))
   {
-    tempoIniciou = 0;//Inicia a variável para a próxima leitura
-    digitalWrite( PIN_GRAVAR, sinalGravar); //Aciona sinal de saída
-    unsigned long tempoDecorrido = ( millis() - tempoIniciou );
-    tempoFinalizar = millis() + tempoDecorrido; //Agenda fim do sinal de saída    
-    Serial.print( " tempo decorrido: " );
-    Serial.print( tempoDecorrido );
-    Serial.println( "ms" );
-    Serial.print( "Repetindo sinal na saida" );
+    delay(5);//debounce
+    if (digitalRead( PIN_LEITURA ) != sinalLer)
+    {
+      if (tempoFinalizar == 0)
+      {
+        digitalWrite( PIN_GRAVAR, sinalGravar); //Aciona sinal de saída
+        unsigned long tempoDecorrido = ( millis() - tempoIniciou );
+        tempoFinalizar = millis() + tempoDecorrido; //Agenda fim do sinal de saída
+        Serial.print( " tempo decorrido: " );
+        Serial.print( tempoDecorrido );
+        Serial.println( "ms" );
+        Serial.println( "Repetindo sinal na saida" );
+      }
+      tempoIniciou = 0;//Inicia a variável para a próxima leitura
+    }
   }
 
   //Finaliza o sinal da saída no tempo agendado
-  if ((tempoFinalizar > 0) && (millis() > tempoFinalizar)) 
+  if ((tempoFinalizar > 0) && (millis() > tempoFinalizar))
   {
     tempoFinalizar = 0;
     digitalWrite( PIN_GRAVAR, !sinalGravar);
